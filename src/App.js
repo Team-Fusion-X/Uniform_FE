086eec1e488b7d6b2e8acbb3ce5fa0ddf6fd3735
YongBin123 <134280533+YongBin123@users.noreply.github.com>
2023-11-24 13:49:59 +0900

    [feat] 회원가입 페이지 추가
    회원가입 페이지 코드입니다!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import './App.css';
 import React from 'react';
+import SignUpPage from './signUpPage.js';
+import SignUpPage2 from './signUpPage2.js';
 import LoginPage from './loginPage.js';
 import { Routes, Route } from 'react-router-dom'
 
@@ -6,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signup2" element={<SignUpPage2 />} />
           <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
