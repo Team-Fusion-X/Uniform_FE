769aab20b83da692737d599d211e4d538be006fa
minseokKR <145015478+minseokKR@users.noreply.github.com>
2023-11-29 19:47:13 +0900

    Feat. 메인화면 이동
    css충돌로인한 App.js 에서 index.js로 이동

--- src/App.js
+++ src/App.js
@@ -1,46 +1,36 @@
 import './App.css';
 import React from 'react';
-import SignUpPage from './signUp/signUpPage.js';
-import SignUpPage2 from './signUp/signUpPage2.js';
-import LoginPage from './login/loginPage.js';
 import { Routes, Route, Link } from 'react-router-dom';
-import FocusAnalysis from './menu/focusAnalysis.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AllAnalysis from './menu/allAnalysis.js';
 import MapAnalysis from './menu/mapAnalysis.js';
 import ChatBot from './menu/chatBot.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MyPage from './myPage/myPage.js';
+import FocusAnalysis from './menu/focusAnalysis.js';
 
 function App() {
   return (
     <div className="App">
       
-      <div className="bar" />
+      {/* <div className="bar" />
       <div className="logo" />
       <div className="mypage-btn">
         <Link to="login">
           로그인/마이페이지
         </Link>
-      </div>
- 
+      </div> */}
       <Routes>
         <Route path="/" element={
-          <div>
-            <div className="menu-container">
-              <FocusAnalysis></FocusAnalysis>
-              <AllAnalysis></AllAnalysis>
-              <MapAnalysis></MapAnalysis>
-              <ChatBot></ChatBot>
-            </div>
-          </div>   
-        }/>
+            <div>
+              <div className="menu-container">
+                <FocusAnalysis></FocusAnalysis>
+                <AllAnalysis></AllAnalysis>
+                <MapAnalysis></MapAnalysis>
+                <ChatBot></ChatBot>
+              </div>
+            </div>   
+          }/>
       </Routes>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/signup2" element={<SignUpPage2 />} />
-      </Routes>
+      
     </div>
   );
 }
