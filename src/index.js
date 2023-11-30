ca8c1a15e22cff41caa831ee0c81bf13943cedf6
minseokKR <145015478+minseokKR@users.noreply.github.com>
2023-11-30 16:59:22 +0900

    Feat. 마이페이지 코드 수정
    GET 요청코드
    마이페이지 내용 수정
    input값 변수 저장
    내입시 정보 이동코드
    수정완료시 POST 요청코드
    axios 라이브러리 추가

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import LoginPage from './login/loginPage.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
       
@@ -28,7 +28,7 @@ root.render(
         
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
